7c5e7726427363286380e25284031e96d81bfbdf
kurtw29 <ckurt37@gmail.com>
2018-12-13 17:43:17 -0800

    login/reg validation

--- public/src/app/login/login.component.ts
+++ public/src/app/login/login.component.ts
@@ -6,10 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+  firstName: string;
+  lastName: string;
+  error: any;
+  email: string;
+  password: string;
+  confirmPassword: string;
 
-  constructor() { }
+  constructor() {
+    this.error = {
+      firstName : null, lastName : null, email: null, password: null, confirmPassword: null
+    }
+   }
 
   ngOnInit() {
+    this.error = "";
   }
 
+  regProc(){
+    console.log("register submitted")
+  }
 }
